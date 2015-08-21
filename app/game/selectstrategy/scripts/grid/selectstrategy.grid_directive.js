345fa9290e14a05272749be9bb2372c2d092bc28
sookinoby <sookinoby@gmail.com>
2015-08-21 12:47:18 -0700

    1. renamed the grid directive to selectStrategyGrid

--- app/game/selectstrategy/scripts/grid/selectstrategy.grid_directive.js
+++ app/game/selectstrategy/scripts/grid/selectstrategy.grid_directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('selectStrategyGrid')
-.directive('grid', function() {
+.directive('gridSelectStrategy', function() {
   return {
     restrict: 'A',
     require: 'ngModel',
